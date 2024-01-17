35cc726a11d54684c34beb5b04fa2deb00daa297
victoria tonkovich <tonkovich.vika@gmail.com>
2024-01-17 23:09:32 +0300

    feat: feat: add file naming solution

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const path = require('path');
+
+fs.readdir(path.join(__dirname, 'secret-folder'), (err, files) => {
+  if (err) throw err;
+
+  files.forEach((file) => {
+    const pathFile = path.join(path.join(__dirname, 'secret-folder'), file);
+
+    fs.stat(pathFile, (err, stats) => {
+      if (err) throw err;
+
+      console.log(
+        `${path.parse(file).name} - ${path.parse(file).ext.slice(1)} - ${
+          stats.size
+        } байт`,
+      );
+    });
+  });
+});
