10a24ae94444e1f2e9bb033552ee8814dfb4e8ee
victoria tonkovich <tonkovich.vika@gmail.com>
2024-01-22 14:50:11 +0300

    feat: add solution for copying files

---
+++ 04-copy-directory/index.js
@@ -0,0 +1,23 @@
+const fs = require('fs').promises;
+const path = require('path');
+
+const pathFolder = path.join(__dirname, 'files');
+const pathCopyFolder = path.join(__dirname, 'files-copy');
+
+async function copyDirectory(pathFolder, pathCopyFolder) {
+  await fs.mkdir(pathCopyFolder, { recursive: true });
+
+  const files = await fs.readdir(pathFolder);
+
+  for (const file of files) {
+    const pathFiles = path.join(pathFolder, file);
+    const pathCopyFiles = path.join(pathCopyFolder, file);
+
+    const stats = await fs.stat(pathFiles);
+
+    if (stats.isFile()) {
+      await fs.copyFile(pathFiles, pathCopyFiles);
+    }
+  }
+}
+copyDirectory(pathFolder, pathCopyFolder);
